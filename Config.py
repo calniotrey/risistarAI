6b1acd2523917939b301d845db9f58fc84265482
Cal <calniotrey@gmail.com>
2019-07-10 00:42:27 +0200

    Add config

---
+++ Config.py
@@ -0,0 +1,30 @@
+import os.path
+
+class Config:
+    def __init__(self):
+        self.domain = ""
+        self.escapeTarget = ""
+    
+    def load():
+        if os.path.isfile("config.txt"):
+            return Config.loadFromFile("config.txt")
+        return Config.loadFromFile("defaultConfig.txt")
+            
+    
+    def loadFromFile(filePath):
+        config = Config()
+        file = open(filePath)
+        l = file.readline()
+        while l != "":
+            l = l.replace("\n", "")
+            if len(l) and l[0] != "#":
+                s = l.split("=")
+                key = s[0].replace(" ", "")
+                value = s[1]
+                if key == "domain":
+                    config.domain = value
+                elif key == "escapeTarget":
+                    config.escapeTarget = [int(x) for x in value.split(":")]
+            l = file.readline()
+        file.close()
+        return config
