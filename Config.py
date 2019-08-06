578ce7712caa7ab3a486ec01a804b3c9f4bc6bd2
Cal Niotrey <calniotrey@gmail.com>
2019-08-06 10:52:45 +0200

    Added with statement to file opening

--- Config.py
+++ Config.py
@@ -26,50 +26,48 @@ class Config:
             config.loadFromFile("config.txt")
         return config
 
-
     def loadFromFile(self, filePath):
-        file = open(filePath)
-        l = file.readline()
-        while l != "":
-            l = l.replace("\n", "")
-            if len(l) and l[0] != "#":
-                s = l.split("=")
-                key = s[0].replace(" ", "")
-                value = s[1]
-                if key == "domain":
-                    self.domain = value
-                elif key == "userAgent":
-                    self.userAgent = value
-                elif key == "activateAutoBuild":
-                    self.activateAutoBuild = value == "True"
-                elif key == "activateAutoFleetScan":
-                    self.activateAutoFleetScan = value == "True"
-                elif key == "activateDefenderDiscordPing":
-                    self.activateDefenderDiscordPing = value == "True"
-                elif key == "activateAutoEvasion":
-                    self.activateAutoEvasion = value == "True"
-                elif key == "robotRatio":
-                    self.robotRatio = int(value)
-                elif key == "robotStartingLevel":
-                    self.robotStartingLevel = int(value)
-                elif key == "minimumTimeBetweenScans":
-                    self.minimumTimeBetweenScans = int(value)
-                elif key == "randomAdditionnalTimeBetweenScans":
-                    self.randomAdditionnalTimeBetweenScans = int(value)
-                elif key == "webhookUrl":
-                    self.webhookUrl = value
-                elif key == "idToPing":
-                    self.idToPing = value
-                elif key == "customAttackNotificationMessage":
-                    self.customAttackNotificationMessage = value
-                elif key == "escapeTarget":
-                    self.escapeTarget = [int(x) for x in value.split(":")]
-                elif key == "minimumSpottingTime":
-                    self.minimumSpottingTime = int(value)
-                elif key == "researchPlanetId":
-                    self.researchPlanetId = int(value)
+        with open(filePath) as file:
             l = file.readline()
-        file.close()
+            while l != "":
+                l = l.replace("\n", "")
+                if len(l) and l[0] != "#":
+                    s = l.split("=")
+                    key = s[0].replace(" ", "")
+                    value = s[1]
+                    if key == "domain":
+                        self.domain = value
+                    elif key == "userAgent":
+                        self.userAgent = value
+                    elif key == "activateAutoBuild":
+                        self.activateAutoBuild = value == "True"
+                    elif key == "activateAutoFleetScan":
+                        self.activateAutoFleetScan = value == "True"
+                    elif key == "activateDefenderDiscordPing":
+                        self.activateDefenderDiscordPing = value == "True"
+                    elif key == "activateAutoEvasion":
+                        self.activateAutoEvasion = value == "True"
+                    elif key == "robotRatio":
+                        self.robotRatio = int(value)
+                    elif key == "robotStartingLevel":
+                        self.robotStartingLevel = int(value)
+                    elif key == "minimumTimeBetweenScans":
+                        self.minimumTimeBetweenScans = int(value)
+                    elif key == "randomAdditionnalTimeBetweenScans":
+                        self.randomAdditionnalTimeBetweenScans = int(value)
+                    elif key == "webhookUrl":
+                        self.webhookUrl = value
+                    elif key == "idToPing":
+                        self.idToPing = value
+                    elif key == "customAttackNotificationMessage":
+                        self.customAttackNotificationMessage = value
+                    elif key == "escapeTarget":
+                        self.escapeTarget = [int(x) for x in value.split(":")]
+                    elif key == "minimumSpottingTime":
+                        self.minimumSpottingTime = int(value)
+                    elif key == "researchPlanetId":
+                        self.researchPlanetId = int(value)
+                l = file.readline()
 
     def getError(self):
         if None in self.__dict__.values():
